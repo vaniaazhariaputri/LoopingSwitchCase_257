b69de5517f605f0ba99913af2a52b1b2c2e9f423
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-03-18 12:09:02 +0700

    membuat kedua

--- file.cpp
+++ file.cpp
@@ -16,3 +16,26 @@ int main()
 {
     cout << " Nilai n = " << n << " Selamat Datang" << endl;
 }
+cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
+
+    cout << "Perulangan for pencacah turun" << endl;
+    // Perulangan dengan for
+    for (n = 5; n> 0; n--)
+    {
+        cout << "Nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhr = " << n;
+    cout << endl;
+
+    cout << "Perulangan while" << endl;
+
+    srand(time(0));
+
+    n = rand() % 10;
+    
+    cout << "Nilai awal n = " << n << endl;
+
+    while (n < 7)
+    {
+        cout << "Nilai n = " << n << endl;
---
+++ file2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    int x;
