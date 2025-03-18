67cd7d754955014911d0dbff3553d4d97868874c
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-03-18 12:20:09 +0700

    membuat file2 kedua

--- file2.cpp
+++ file2.cpp
@@ -8,3 +8,14 @@ int main(){
     int perulanganWhile = 0;
     int perulnganDoWhile = 0;
     // Perulangan while
+    cout << "perulangan dengan while" << endl;
+    srand(time(0));
+    x = rand() % 10;
+    cout << "Nilai awal x = " << x << endl;
+    while(x < 7){
+        cout << "nilai x = " << x << "Lari keliling lapangan!" << endl;
+        x = rand() % 10;
+        perulanganWhile++;
+    }
+    cout << "Nilai akhir x = " << x << endl;
+    
