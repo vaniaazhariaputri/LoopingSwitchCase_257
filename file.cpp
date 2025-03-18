6a2a1d4f2d1528fe7f763b9d5d881794677af5e2
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-03-18 11:57:47 +0700

    first commit

---
+++ file.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+#include <cstdLib>
+using namespace std;
+
+int main()
+{
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+    // Perulangan dengan for
+
+    cout << "Perulangan for pencacah naik" << endl;
+    for (n = 0; n< 5; n++)
+{
+    cout << " Nilai n = " << n << " Selamat Datang" << endl;
+}
