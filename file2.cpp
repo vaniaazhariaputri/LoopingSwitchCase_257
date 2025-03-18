1f4b4b9091a074512edec4c5734ef67decb1dc7b
vaniaazhariaputri <vaniaazharia@gmail.com>
2025-03-18 12:28:45 +0700

    membuat file 2 ketiga

--- file2.cpp
+++ file2.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 using namespace std;
 
+float luasPersegiPanjang(float a, float b){
+    return a * b;
+}
+float luasLingkaran(float a) {
+    return 3.14*a*a;
+}
+
 int main(){
-    int x;
-    int perulanganWhile = 0;
-    int perulnganDoWhile = 0;
-    // Perulangan while
-    cout << "perulangan dengan while" << endl;
-    srand(time(0));
-    x = rand() % 10;
-    cout << "Nilai awal x = " << x << endl;
-    while(x < 7){
-        cout << "nilai x = " << x << "Lari keliling lapangan!" << endl;
-        x = rand() % 10;
-        perulanganWhile++;
-    }
-    cout << "Nilai akhir x = " << x << endl;
-    
+    int pilihan;
+    float panjang, lebar;
+    float jejari;
+
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Liangkaran" << endl;
+        cout << "3. Keluar" << endl;
+
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;
+
+        switch (pilihan)
